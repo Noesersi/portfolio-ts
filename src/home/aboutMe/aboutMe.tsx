ebd3d3613bdbc97fef4807593637a0ab44fcd3f4
Noé Ibáñez Torres <noesersi@gmail.com>
2023-12-20 23:02:36 +0100

    fixing rough notation

--- src/home/aboutMe/aboutMe.tsx
+++ src/home/aboutMe/aboutMe.tsx
@@ -8,18 +8,18 @@ const AboutMe: React.FC = () => {
     <div className={styles.aboutMeContainer}>
       <div className={styles.profileContainer}>
         <h1 className={styles.name} >Noé Ibáñez Torres</h1>
-        <h5 className={styles.profession}>Full Stack Developer</h5>
+        <h5 className={styles.profession}><RoughNotation type='bracket' color='black' brackets={['left', 'right']} show={true}>Full Stack Developer</RoughNotation></h5>
         <img src={Perfil} alt="Foto de perfil" className={styles.profileImage} />
         <div className={styles.description}>
           <p>
-            Results-driven{' '}<RoughNotation type="highlight" color="yellow" show={true}>Full Stack Developer</RoughNotation> with a passion for technology, specialized in crafting impactful projects by using a full stack approach and technologies such as{' '}
-            <RoughNotation type="circle" color="black" show={true}>
+          <RoughNotation type="underline" color="black" padding={0} show={true}>Results-driven</RoughNotation>{' '}Full Stack Developer with a passion for technology, specialized in crafting impactful projects by using a full stack approach and technologies such as{' '}
+            <RoughNotation type="circle" color="black" padding={3} show={true}>
               JavaScript
             </RoughNotation> and{' '}
-            <RoughNotation type="circle" color="black" show={true}>
+            <RoughNotation type="circle" color="black" padding={4} show={true}>
               Python
             </RoughNotation>.</p> <p> I am particularly enthusiastic about{' '}
-            <RoughNotation type='highlight' color='yellow' show={true}>clean coding practices</RoughNotation>, ensuring that my code is not only functional but also maintainable and easy to understand.
+            <RoughNotation type='highlight' color='yellow' multiline={true} show={true}>clean coding practices</RoughNotation>, ensuring that my code is not only functional but also maintainable and easy to understand.
           </p>
         </div>
 
